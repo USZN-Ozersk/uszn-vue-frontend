2a76117c8a91741aae9f002ebada3ae77a8a9447
dimas volkov <dimas.volkov@gmail.com>
2020-02-12 17:51:04 +0500

    dimas.volkov@gmail.com

--- src/components/mainPage/topNewsFirst.vue
+++ src/components/mainPage/topNewsFirst.vue
@@ -2,7 +2,7 @@
     <v-card white raised class="d-flex flex-column pa-3 mr-1 ml-1 mb-2" min-height="533">
       <span class="headline font-weight-light left pb-3">Новости управления</span>
       <v-card class="d-flex flex-column" tile flat>
-              <v-img :src="require('../../assets/'+getFirstNews[0].news_img)" aspect-ratio="2.5"></v-img>
+              <v-img v-bind:src="getFirstNews[0].news_img" aspect-ratio="2.5"/>
               <v-card-title>
                   <span class="text-xs-left">{{ getFirstNews[0].news_name }}</span>
               </v-card-title>
--- src/views/newsPage.vue
+++ src/views/newsPage.vue
@@ -3,7 +3,7 @@
         <v-row justify="center" v-if="!getBadVision">
             <v-col cols="12" lg="7">
                 <v-card flat class="d-flex flex-column blue-grey lighten-5">
-                    <v-img :src="require('../assets/'+getOneNews.news_img)" aspect-ratio="1.3"></v-img>
+                    <img v-bind:src="getOneNews.news_img" aspect-ratio="1.3"/>
                     <v-card-title>{{ getOneNews.news_name }}</v-card-title>
                     <div v-html="getOneNews.news_text"></div>
                 </v-card>
@@ -12,7 +12,7 @@
               <div class="pb-3"><span class="subtitle-1">Последние новости</span></div>
               <div class="d-flex flex-column pb-3" v-for="news in getFirstNews" :key="news.news_id">
                     <v-card class="d-flex flex-column">
-                      <v-img :src="require('../assets/'+news.news_img)" aspect-ratio="2.4"></v-img>
+                      <v-img v-bind:src="news.news_img" aspect-ratio="2.4"/>
                         <v-card-subtitle>
                           <router-link style="text-decoration: none; color: black;" :to="{ name: 'news', params: { id: news.news_id }}">{{ news.news_name }}</router-link>
                         </v-card-subtitle>
@@ -24,7 +24,7 @@
           <v-col cols="12" lg="10">
             <div class="d-flex flex-column flex-grow-1">
               <span class="text-center pb-2" v-bind:class="{'subtitle-2' : getBvParams.bvFont == 'small', 'title' : getBvParams.bvFont == 'medium', 'display-1' : getBvParams.bvFont == 'large'}">{{ getOneNews.news_name }}</span>
-            <div v-if="getBvParams.bvImages"><v-img :src="require('../assets/'+getOneNews.news_img)" aspect-ratio="1.3"></v-img></div>
+            <div v-if="getBvParams.bvImages"><v-img :src="getOneNews.news_img" aspect-ratio="1.3"></v-img></div>
             <span v-html="getOneNews.news_text" v-bind:class="{'hide' : !getBvParams.bvImages, 'body-2' : getBvParams.bvFont == 'small', 'body-1' : getBvParams.bvFont == 'medium', 'title' : getBvParams.bvFont == 'large'}"/>
             </div>
           </v-col>

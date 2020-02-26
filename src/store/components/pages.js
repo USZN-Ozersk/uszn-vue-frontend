8f77588068436b9156670618d01bc5fe92a4edf8
dimas volkov <dimas.volkov@gmail.com>
2020-02-26 17:48:05 +0500

    work

--- src/store/components/pages.js
+++ src/store/components/pages.js
@@ -5,12 +5,15 @@ const path = "http://localhost:8080/api/v1/"
 export default {
     state: {
         currentPage: '',
+        allPages: ['']
     },
     getters: {
         getOnePage: state => {return state.currentPage}, // Геттер содержимого страницы
+        getAllPages: state => {return state.allPages}
     },
     mutations: {
         setCurrentPage: (state, payload) => {state.currentPage = payload}, // Мутация состояния содержимого страницы
+        setAllPages: (state, payload) => {state.allPages = payload}
     },
     actions: {
         loadOnePage: (context, id) => {                                             // Действие для загрузки и мутации содержимого страницы
@@ -23,5 +26,18 @@ export default {
                 context.commit('setError', error)
               })
         },
+        loadAllPages: (context, , , rootGetters) => {
+            console.log(rootGetters.getToken)
+            axios
+                .get(path+'private/pages', {
+                    headers: {
+                        "Token": context.rootGetters['auth/getToken']
+                    }
+                }
+            )
+            .then(response => {
+                context.commit('setAllPages', response)
+            })
+        }
     }
 }
--- src/views/pageAdmin.vue
+++ src/views/pageAdmin.vue
@@ -5,11 +5,11 @@
                <v-card>
                    <v-card-text>
                         <v-toolbar color="primary" dark flat>
-                            <v-toolbar-title>Login form</v-toolbar-title>
+                            <v-toolbar-title>Вход</v-toolbar-title>
                         </v-toolbar>
                         <v-form>
-                            <v-text-field v-model="userdata.login" label="Login" name="login" prepend-icon="fas fa-user" type="text" />
-                            <v-text-field v-model="userdata.password" label="Password" name="password" prepend-icon="fas fa-lock" type="password" />
+                            <v-text-field v-model="userdata.login" label="Login" prepend-icon="fas fa-user" type="text" />
+                            <v-text-field v-model="userdata.password" label="Password" prepend-icon="fas fa-lock" type="password" />
                             <span class="red--text" v-if="getAuthError != ''">Ошибка авторизации: "{{ getAuthError }}".</span>
                         </v-form>
                    </v-card-text>
@@ -21,7 +21,7 @@
            </v-col>
        </v-row>
 
-       <v-row>
+       <v-row v-if="getAuth">
            <v-col>
                <div class="d-flex flex-row">
                 <v-tabs v-model="tab">
@@ -30,19 +30,61 @@
                     <v-tab key="3">Страницы</v-tab>
                     <v-tab-item key="1">
                         <div class="d-flex flex-row">
-                        <v-treeview expand-icon="fa fa-angle-right" return-object activatable active-class="primary--text" :active.sync="active" color="blue" :items="getTreeMenu"></v-treeview>
-                        <div>
-                            <v-card class="pa-3">
-                                {{ selected[0].id }}
-                                <v-text-field v-model="menudata.menu_id"></v-text-field>
-                                <v-text-field v-model="menudata.menu_item"></v-text-field>
-                                <v-text-field v-model="menudata.menu_parent"></v-text-field>
-                            </v-card>
-                        </div>
+                        <v-card flat class="d-flex mr-3" min-width="30%">
+                        <v-treeview @update:active="setMenuFrom" expand-icon="fa fa-angle-right" return-object activatable active-class="primary--text" :active.sync="active" color="blue" :items="getTreeMenu"></v-treeview>
+                        </v-card>
+                        <v-card flat class="pa-3" min-width="30%">
+                            <v-card-text>
+                                <v-form>
+                                    <v-toolbar color="primary" dark flat>
+                                        <v-toolbar-title>Меню</v-toolbar-title>
+                                    </v-toolbar>
+                                    <v-text-field v-model="menudata.menu_id"></v-text-field>
+                                    <v-text-field v-model="menudata.menu_item"></v-text-field>
+                                    <v-text-field v-model="menudata.menu_parent"></v-text-field>
+                                </v-form>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-btn color="success" :disabled="menuAdd || !menuValid">Добавить</v-btn>
+                                <v-spacer />
+                                <v-btn color="warning" :disabled="!menuAdd || !menuValid">Изменить</v-btn>
+                                <v-btn color="error" :disabled="!menuAdd || !menuValid">Удалить</v-btn>
+                            </v-card-actions>
+                        </v-card>
                         </div>
                     </v-tab-item>
-                    <v-tab-item key="2">Новости</v-tab-item>
-                    <v-tab-item key="3">Страницы</v-tab-item>
+                    <v-tab-item key="2">
+                        <div class="d-flex flex-row">
+                            <v-card class="d-flex flex-column pa-3 mr-3" min-width="20%">
+                                <div class="d-flex flex-row" v-for="news in getNewsPage" :key="news.news_id">
+                                    <a @click.prevent="setNewsForm(news.news_id)">{{ news.news_name }}</a>
+                                </div>
+                            </v-card>
+                            <v-card min-width="60%">
+                                <v-card-text>
+                                    <v-form>
+                                        <v-toolbar color="primary" dark flat>
+                                            <v-toolbar-title>Новости</v-toolbar-title>
+                                        </v-toolbar>
+                                        <v-text-field v-model="newsdata.news_id"></v-text-field>
+                                        <v-text-field v-model="newsdata.news_name"></v-text-field>
+                                        <v-textarea rows="13" v-model="newsdata.news_text"></v-textarea>
+                                        <v-text-field v-model="newsdata.news_img"></v-text-field>
+                                    </v-form>
+                                </v-card-text>
+                                <v-card-actions>
+                                    <v-btn color="success" :disabled="newsAdd || !newsValid">Добавить</v-btn>
+                                    <v-spacer />
+                                    <v-btn color="warning" :disabled="!newsAdd || !newsValid">Изменить</v-btn>
+                                    <v-btn color="error" :disabled="!newsAdd || !newsValid">Удалить</v-btn>
+                                </v-card-actions>
+                            </v-card>
+                            
+                        </div>
+                    </v-tab-item>
+                    <v-tab-item key="3">
+                        {{ getAllPages.page_name }}
+                    </v-tab-item>
                 </v-tabs>
                 <v-btn @click="logout">Выход</v-btn>
                </div>
@@ -65,18 +107,49 @@ export default {
                 menu_item: '',
                 menu_parent: ''
             },
+            newsdata: {
+                news_id: '',
+                news_name: '',
+                news_text: '',
+                news_img: ''
+            },
             tab: null,
-            active: [],
+            active: [{id: '', name: '', parent: ''}],
         }
     },
   computed: {
-    ...mapGetters(['getAuth', 'getAuthError', 'getTreeMenu']),
-    selected() {
-        return this.active;
+    ...mapGetters(['getAuth', 'getAuthError', 'getTreeMenu', 'getNewsPage', 'getOneNews', 'getAllPages']),
+    menuValid() {
+        return this.menudata.menu_item
+    },
+    menuAdd() {
+        return this.menudata.menu_id
+    },
+    newsValid() {
+        return this.newsdata.news_name && this.newsdata.news_text && this.newsdata.news_img
+    },
+    newsAdd() {
+        return this.newsdata.news_id
     }
   },
   methods: {
-    ...mapActions(['authorize', 'logout'])
+    ...mapActions(['authorize', 'logout', 'loadNewsPage', 'loadOneNews', 'loadAllPages']),
+    setMenuFrom() {
+        this.menudata.menu_id = this.active[0].id
+        this.menudata.menu_item = this.active[0].name
+        this.menudata.menu_parent = this.active[0].parent
+    },
+    setNewsForm(id) {
+        this.loadOneNews(id)
+        this.newsdata.news_id = this.getOneNews.news_id
+        this.newsdata.news_name = this.getOneNews.news_name
+        this.newsdata.news_text = this.getOneNews.news_text
+        this.newsdata.news_img = this.getOneNews.news_img
+    }
+  },
+  created() {
+      this.loadNewsPage(1),
+      this.loadAllPages()
   }
 }
 </script>

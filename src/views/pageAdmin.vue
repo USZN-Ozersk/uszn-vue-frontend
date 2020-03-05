5147627cf25f09d42aeb702d6dcb2cdc7790d54c
dimas volkov <dimas.volkov@gmail.com>
2020-03-05 16:36:05 +0500

    work

--- src/views/pageAdmin.vue
+++ src/views/pageAdmin.vue
@@ -24,50 +24,42 @@
        <v-row v-if="getAuth">
            <v-col>
                <div class="d-flex flex-row">
-                <v-tabs v-model="tab">
+                <v-tabs background-color="#d4cdc4" v-model="tab">
                     <v-tab key="1">Меню</v-tab>
                     <v-tab @change="loadNewsPage(1)" key="2">Новости</v-tab>
                     <v-tab @change="loadAllPages()" key="3">Страницы</v-tab>
                     <v-tab-item key="1">
-                        <div class="d-flex flex-row">
-                        <v-card flat class="d-flex mr-3" min-width="30%">
+                        <v-card flat class="d-flex flex-row" color="#d4cdc4">
+                        <v-card class="d-flex mr-3" min-width="30%">
                         <v-treeview @update:active.capture="setMenuForm" expand-icon="fa fa-angle-right" return-object activatable active-class="primary--text" :active.sync="active" color="blue" :items="getTreeMenu"></v-treeview>
                         </v-card>
-                        <v-card flat class="pa-3" min-width="30%">
+                        <v-card class="pa-3" min-width="30%">
                             <v-card-text>
                                 <v-form>
-                                    <v-toolbar color="primary" dark flat>
-                                        <v-toolbar-title>Меню</v-toolbar-title>
-                                    </v-toolbar>
-                                    <br>
                                     <v-text-field outlined label="id" v-model="menudata.menu_id"></v-text-field>
                                     <v-text-field outlined label="Название" v-model="menudata.menu_item"></v-text-field>
                                     <v-text-field outlined label="Родительский" v-model="menudata.menu_parent"></v-text-field>
                                 </v-form>
                             </v-card-text>
                             <v-card-actions>
-                                <v-btn @click="insertMenuItem(menudata)" color="success" :disabled="menuAdd || !menuValid">Добавить</v-btn>
+                                <v-btn @click="insertMenuItem(menudata), clearMenu()" color="success" :disabled="menuAdd || !menuValid">Добавить</v-btn>
                                 <v-spacer />
-                                <v-btn @click="updateMenuItem(menudata)" color="warning" :disabled="!menuAdd || !menuValid">Изменить</v-btn>
-                                <v-btn @click="deleteMenuItem(menudata)" color="error" :disabled="!menuAdd || !menuValid">Удалить</v-btn>
+                                <v-btn @click="updateMenuItem(menudata), clearMenu()" color="warning" :disabled="!menuAdd || !menuValid">Изменить</v-btn>
+                                <v-btn @click="deleteMenuItem(menudata), clearMenu()" color="error" :disabled="!menuAdd || !menuValid">Удалить</v-btn>
                             </v-card-actions>
                         </v-card>
-                        </div>
+                        </v-card>
                     </v-tab-item>
                     <v-tab-item key="2">
-                        <div class="d-flex flex-row">
+                        <v-card flat color="#d4cdc4" class="d-flex flex-row">
                             <v-card class="d-flex flex-column pa-3 mr-3" min-width="20%">
                                 <div class="d-flex" v-for="news in getNewsPage" :key="news.news_id">
                                     <a @click.prevent="setNewsForm(news.news_id)">{{ news.news_name }}</a>
                                 </div>
                             </v-card>
-                            <v-card min-width="60%">
+                            <v-card min-width="50%">
                                 <v-card-text>
                                     <v-form>
-                                        <v-toolbar color="primary" dark flat>
-                                            <v-toolbar-title>Новости</v-toolbar-title>
-                                        </v-toolbar>
-                                        <br>
                                         <div class="d-flex flex-row">
                                         <v-text-field outlined class="mr-3" label="id" v-model="newsdata.news_id"></v-text-field>
                                         <v-text-field outlined label="Изображение" v-model="newsdata.news_img"></v-text-field>
@@ -77,45 +69,41 @@
                                     </v-form>
                                 </v-card-text>
                                 <v-card-actions>
-                                    <v-btn @click="insertNews(newsdata)" color="success" :disabled="newsAdd || !newsValid">Добавить</v-btn>
+                                    <v-btn @click="insertNews(newsdata), clearNews()" color="success" :disabled="newsAdd || !newsValid">Добавить</v-btn>
                                     <v-spacer />
-                                    <v-btn @click="updateNews(newsdata)" color="warning" :disabled="!newsAdd || !newsValid">Изменить</v-btn>
-                                    <v-btn @click="deleteNews(newsdata)" color="error" :disabled="!newsAdd || !newsValid">Удалить</v-btn>
+                                    <v-btn @click="updateNews(newsdata), clearNews()" color="warning" :disabled="!newsAdd || !newsValid">Изменить</v-btn>
+                                    <v-btn @click="deleteNews(newsdata), clearNews()" color="error" :disabled="!newsAdd || !newsValid">Удалить</v-btn>
                                 </v-card-actions>
                             </v-card>
                             
-                        </div>
+                        </v-card>
                     </v-tab-item>
                     <v-tab-item key="3">
-                        <div class="d-flex flex-row">
-                            <v-card class="d-flex flex-column pa-3 mr-3" min-width="30%">
+                        <v-card flat color="#d4cdc4" class="d-flex flex-row">
+                            <v-card class="d-flex flex-column pa-3 mr-3" min-width="20%">
                                 <div class="d-flex" v-for="page in getAllPages" :key="page.page_id">
                                     <a @click.prevent="setPagesForm(page.page_menu)">{{ page.page_name }}</a>
                                 </div>
                             </v-card>
-                            <v-card min-width="70%" class="mr-3">
+                            <v-card min-width="50%" class="mr-3">
                                 <v-card-text>
                                     <v-form>
-                                        <v-toolbar color="primary" dark flat>
-                                            <v-toolbar-title>Страницы</v-toolbar-title>
-                                        </v-toolbar>
-                                        <br>
                                         <div class="d-flex flex-row">
                                             <v-text-field class="mr-3" outlined label="id" v-model="pagedata.page_id"></v-text-field>
                                             <v-text-field outlined label="Пункт меню" v-model="pagedata.page_menu"></v-text-field>
                                         </div>
                                         <v-text-field outlined label="Заголовок" v-model="pagedata.page_name"></v-text-field>
-                                        <vue-editor v-model="pagedata.page_text" id="page"></vue-editor>
+                                        <vue-editor useCustomImageHandler @image-added="handleImageAdded" v-model="pagedata.page_text" id="page"></vue-editor>
                                     </v-form>
                                 </v-card-text>
                                 <v-card-actions>
-                                    <v-btn @click="insertPage(pagedata)" color="success" :disabled="pageAdd || !pageValid">Добавить</v-btn>
+                                    <v-btn @click="insertPage(pagedata), clearPages()" color="success" :disabled="pageAdd || !pageValid">Добавить</v-btn>
                                     <v-spacer />
-                                    <v-btn @click="updatePage(pagedata)" color="warning" :disabled="!pageAdd || !pageValid">Изменить</v-btn>
-                                    <v-btn @click="deletePage(pagedata)" color="error" :disabled="!pageAdd || !pageValid">Удалить</v-btn>
+                                    <v-btn @click="updatePage(pagedata), clearPages()" color="warning" :disabled="!pageAdd || !pageValid">Изменить</v-btn>
+                                    <v-btn @click="deletePage(pagedata), clearPages()" color="error" :disabled="!pageAdd || !pageValid">Удалить</v-btn>
                                 </v-card-actions>
                             </v-card>
-                        </div>
+                        </v-card>
                     </v-tab-item>
                 </v-tabs>
                 
@@ -208,6 +196,15 @@ export default {
         .catch(err => {
           console.log(err);
         })
+    },
+    clearMenu() {
+        this.menudata.menu_id = this.menudata.menu_item = this.menudata.menu_parent = ''
+    },
+    clearNews() {
+        this.newsdata.news_id = this.newsdata.news_name = this.newsdata.news_text = this.newsdata.news_img = ''
+    },
+    clearPages() {
+        this.pagedata.page_id = this.pagedata.page_name = this.pagedata.page_text = this.pagedata.page_menu = ''
     },
     setMenuForm() {
         if (this.active[0]) {

a8284fe27e8e11d368c3d80155e510fbf82c1929
Dimas Volkov <dimas.volkov@gmail.com>
2020-04-17 10:36:46 +0500

    work

--- src/views/customPages/officials.vue
+++ src/views/customPages/officials.vue
@@ -9,7 +9,9 @@
                 <v-card tile flat min-height="100%" class="d-flex flex-column">
                     <v-card-title class="display-1">Солодовникова<br>Лилия<br>Владимировна</v-card-title>
                     <v-card-subtitle class="title">Начальник Управления</v-card-subtitle>
-                    <v-card-text>Прием по личным вопросам: ...</v-card-text>
+                    <v-card-text class="subtitle-1">Образование: высшее.<br>В сфере социальной защиты населения работает с ??? года.</v-card-text>
+                    <v-card-text class="subtitle-1">Телефон: (35130) 4-53-32</v-card-text>
+                    <v-card-text class="subtitle-1">Прием по личным вопросам каждый четверг с 9:00 до 17:00 по предварительной записи.<br>Запись по телефону: (35130)5-66-84.</v-card-text>
                 </v-card>
             </v-card>
         </v-col>
@@ -21,6 +23,9 @@
                 <v-card tile flat>
                     <v-card-title class="display-1">Петренко<br>Татьяна<br>Александровна</v-card-title>
                     <v-card-subtitle class="title">Заместитель начальника</v-card-subtitle>
+                    <v-card-text class="subtitle-1">Образование: высшее.<br>В сфере социальной защиты населения работает с 1999 года.</v-card-text>
+                    <v-card-text class="subtitle-1">Телефон: (35130) 2-46-07</v-card-text>
+                    <v-card-text class="subtitle-1">Прием по личным вопросам каждый вторник с 9:00 до 17:00 по предварительной записи.<br>Запись по телефону: (35130)5-66-84.</v-card-text>
                 </v-card>
             </v-card>
         </v-col>

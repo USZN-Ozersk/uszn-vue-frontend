6bf2b5b89966148c391049e17fca344a188d9266
Dimas Volkov <dimas.volkov@gmail.com>
2020-04-20 11:01:02 +0500

    work

--- src/views/customPages/officials.vue
+++ src/views/customPages/officials.vue
@@ -9,7 +9,7 @@
                 <v-card tile flat min-height="100%" class="d-flex flex-column">
                     <v-card-title class="display-1">Солодовникова<br>Лилия<br>Владимировна</v-card-title>
                     <v-card-subtitle class="title">Начальник Управления</v-card-subtitle>
-                    <v-card-text class="subtitle-2">Образование: высшее.<br>В сфере социальной защиты населения работает с ??? года.</v-card-text>
+                    <v-card-text class="subtitle-2">Образование: высшее.<br>В сфере социальной защиты населения работает с 1994 года.</v-card-text>
                     <v-card-text class="subtitle-2">Телефон: (35130) 4-53-32</v-card-text>
                     <v-card-text class="subtitle-2">Прием по личным вопросам каждый вторник с 9:00 до 17:00 по предварительной записи.<br>Запись по телефону: (35130)5-66-84.</v-card-text>
                 </v-card>
@@ -33,7 +33,7 @@
 
     <v-row no-gutters justify="center" v-if="getBadVision">
          <v-col cols="12" lg="5" sm="12">
-            <v-card flat tile class="d-flex flex-row ma-2">
+            <v-card flat tile class="d-flex flex-row ma-2" v-bind:class="getBvParams.bvBackground">
                 <v-card tile flat min-width="50%" max-width="50%" v-if="getBvParams.bvImages">
                     <v-img aspect-ratio="0.7" :src="require('../../assets/boss.jpg')"></v-img>
                 </v-card>
@@ -47,7 +47,7 @@
             </v-card>
         </v-col>
         <v-col cols="12" lg="5" sm="12">
-            <v-card flat tile class="d-flex flex-row ma-2">
+            <v-card flat tile class="d-flex flex-row ma-2" v-bind:class="getBvParams.bvBackground">
                 <v-card tile flat min-width="50%" max-width="50%" v-if="getBvParams.bvImages">
                     <v-img aspect-ratio="0.7" :src="require('../../assets/boss2.jpg')"></v-img>
                 </v-card>

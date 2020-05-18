e8d3e8ab2fc071a4c10b63be7d0e7345030db7e3
Dimas Volkov <dimas.volkov@gmail.com>
2020-05-18 11:40:18 +0500

    work

--- src/components/mainPage/topPhones.vue
+++ src/components/mainPage/topPhones.vue
@@ -1,7 +1,7 @@
 <template>
     <div min-height="100%" class="d-flex flex-column justify-space-between pa-2">
         <span class="text-xs-left subheading font-weight-regular">Важные телефоны:</span>
-        <span class="text-xs-left subheading font-weight-light">(35130)6-66-84 - Приемная</span>
-        <span class="text-xs-left subheading font-weight-light">(35130)2-17-93 - Отдел опеки</span>
+        <span class="text-xs-left subheading font-weight-light">(35130)5-66-84 - Приемная</span>
+        <span class="text-xs-left subheading font-weight-light">(35130)9-11-08 - Отдел опеки</span>
     </div>
 </template>

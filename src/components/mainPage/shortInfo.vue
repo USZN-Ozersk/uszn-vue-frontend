39a54073931a8dc549bb4fe46e5de29df5080c2d
Dimas Volkov <dimas.volkov@gmail.com>
2020-10-19 11:40:48 +0500

    work

---
+++ src/components/mainPage/shortInfo.vue
@@ -0,0 +1,70 @@
+<template>
+<v-row no-gutters>
+    <v-col cols="12" lg="3" sm="12" v-for="info in infoblock" :key="info.id">
+       <!-- <v-card class="d-flex ma-1 pa-2">
+            <v-card flat tile min-width="50%" max-width="50%"><v-img aspect-ratio="0.8" :alt="info.alt" :src="require('../../assets/'+info.img)"></v-img></v-card>
+            <v-card flat tile class="ml-2">
+                <p class="subtitle-1">{{info.text1}}</p>
+                <p class="font-weight-medium">{{info.text2}}</p>
+                <p class="font-weight-medium">{{info.text3}}</p>
+            </v-card>
+        </v-card> -->
+        <v-card class="ma-1">
+            <v-img class="align-end" aspect-ratio="1.5" :alt="info.alt" :src="require('../../assets/'+info.img)">
+                <v-card class="white--text" flat tile color="rgb(0, 0, 0, 0.5)">
+                   <!-- <p class="title pa-3 ma-4">{{ info.text1 }}</p> -->
+                   <v-card-title>{{info.text0}}<br>{{info.text1}}</v-card-title>
+                </v-card>
+            </v-img>
+                <v-card-subtitle class="title font-weight-bold">{{info.text2}}<br>{{info.text3}}</v-card-subtitle>
+        </v-card>
+    </v-col>
+</v-row>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            infoblock: [
+                {
+                    id: 1,
+                    img: "rojdenie.jpg",
+                    alt: "Единовременное пособие при рождении ребенка",
+                    text0: "Единовременное пособие при",
+                    text1: "рождении ребенка",
+                    text2: "294 семьи",
+                    text3: "880 381,50 руб.",
+                },
+                {
+                    id: 2,
+                    img: "pervii.jpg",
+                    alt: "Ежемесячная выплата при рождении первого ребенка",
+                    text0: "Ежемесячная выплата при",
+                    text1: "рождении первого ребенка",
+                    text2: "350 семей",
+                    text3: "25 808 435,52 руб.",
+                },
+                {
+                    id: 3,
+                    img: "tretii.jpg",
+                    alt: "Ежемесячная выплата при рождении третьего ребенка",
+                    text0: "Ежемесячная выплата при",
+                    text1: "рождении третьего ребенка",
+                    text2: "188 семей",
+                    text3: "13 199 585,54 руб.",
+                },
+                {
+                    id: 4,
+                    img: "matkapit.jpg",
+                    alt: "Региональный материнский капитал",
+                    text0: "Региональный материнский",
+                    text1: "капитал",
+                    text2: "31 семья",
+                    text3: "3 100 000 руб.",
+                }
+            ]
+        }
+    }
+}
+</script>
--- src/views/mainPage.vue
+++ src/views/mainPage.vue
@@ -39,7 +39,7 @@
     </v-row>
 
     <!-- Вторая строка блоков - Новости и руководитель -->
-    <v-row class="pt-3" no-gutters justify="center" v-if="!getBadVision">
+    <v-row class="pt-2" no-gutters justify="center" v-if="!getBadVision">
       <v-col cols="12" lg="4" md="5" sm="12">
         <topNewsFirst></topNewsFirst>    
       </v-col>
@@ -57,9 +57,22 @@
       </v-col>
     </v-row>
 
-    <!-- Третья строка блоков - Баннеры -->
-    <v-row class="pt-3" no-gutters justify="center" v-if="!getBadVision">
+    <!-- Третья строка блоков - Короткая информация -->
+    <v-row class="pt-2" no-gutters justify="center" v-if="!getBadVision">
       <v-col cols="12" lg="10" sm="12">
+        <div class="d-flex pa-2 align-self-center">
+        <span class="title font-weight-light">Управление социальной защиты населения в цифрах: назначение детских пособий по состоянию на 1 октября 2020 года.</span>
+        </div>
+        <shortInfo></shortInfo>
+      </v-col>
+    </v-row>
+
+    <!-- Четвертая строка блоков - Баннеры -->
+    <v-row class="pt-2" no-gutters justify="center" v-if="!getBadVision">
+      <v-col cols="12" lg="10" sm="12">
+        <div class="d-flex pa-2 align-self-center">
+        <span class="title font-weight-light">Полезные ссылки:</span>
+        </div>
         <banners></banners>
       </v-col>
     </v-row>
@@ -79,6 +92,7 @@ components: {
   topNewsOthers: () => import('@/components/mainPage/topNewsOthers'), // Новости на главной странице
   topNewsBV: () => import('@/components/mainPage/topNewsBV'), // Новости на главной странице для слабовидящих
   topBoss: () => import('@/components/mainPage/topBoss'), // Инфо о начальнике
+  shortInfo: () => import('@/components/mainPage/shortInfo'),
   banners:() => import('@/components/mainPage/banners'),
   //mysnackbar:() => import('@/components/mainPage/snackbar'),
 },
